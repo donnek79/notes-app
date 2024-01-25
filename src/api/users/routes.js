bc6c3f85b472578ef02e65d1744eafdb1aa0c3d5
donnek79 <pras.wahyu.79@gmail.com>
2024-01-25 21:55:54 +0700

    collaboration implemented

--- src/api/users/routes.js
+++ src/api/users/routes.js
@@ -9,6 +9,11 @@ const routes = (handler) => [
     path: '/users/{id}',
     handler: (request) => handler.getUserByIdHandler(request),
   },
+  {
+    method: 'GET',
+    path: '/users',
+    handler: (request, h) => handler.getUsersByUsernameHandler(request, h),
+  },
 ];
 
 module.exports = routes;
